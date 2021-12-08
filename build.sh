62e4fd919f741afd44ebf920173b8c6c7696b69b
Luca Calacci <chipscal@gmail.com>
2021-12-08 13:16:27 +0100

    script fix

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-git submodule init --recursive
-git apply --index
-git submodule update
+git submodule update --init --recursive
+cd chirpstack-api
+git apply --index ../patches/0001-chirpstack_csharp_patch.patch
+cd ..
 
 dotnet build ./chirpstack-api/ChirpStack.Generate.csproj
 dotnet build -c Release ./ChirpStack.Api/ChirpStack.Api.csproj
